871da8a2251d085747f479350416f6542d7c7531
Artemiy Schukin <shuckinart@yandex.ru>
2021-10-08 17:19:44 +0300

    up deps

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       malinaPlugin({
         debugLabel: DEV,
+        hideLabel: !DEV
       }),
       vanillaExtractPlugin(),
       !DEV && viteSingleFile(),
